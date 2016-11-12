14ada9395aeed5bde4abf2b17f0af301817c6738
Collin Schneider <collin@jastr.co>
2016-11-12 11:02:48 -0500

    setTimeout

--- weather.js
+++ weather.js
@@ -1,49 +1,51 @@
 #!/usr/bin/env node
 var request = require('request');
 var figlet = require('figlet');
+var moment = require('moment')
 require('dotenv').config();
+var readline = require('readline');
 
-if(process.argv[2]){
-  var city = process.argv[2].replace(' ', '')
-  var state = process.argv[3]
-  var query = state === undefined ? city : city + ',' + state.replace(' ', '')
-  // var url = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+'&cnt=7&appid='+process.env.API_KEY; FORECAST
-  var url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+process.env.API_KEY;
-  getWeather(url)
-} else {
-  figlet('Weather-Please', function(err, msg) {
-      if (err) {
-        console.log('error...');
-        console.dir(err);
-        return;
-      }
-      console.log(msg)
-      console.log("Use command 'weather-please <city(zip or name)>' to retrieve weather info on your city\n");
-  });
-}
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
+rl.question("Zip code of desired weather: ", function(zip) {
+  var forecast = 'http://api.openweathermap.org/data/2.5/forecast/daily?q='+zip+'&cnt=7&units=imperial&appid='+process.env.API_KEY;
+  var current = 'http://api.openweathermap.org/data/2.5/weather?q='+zip+'&units=imperial&appid='+process.env.API_KEY;
+  getCurrentWeather(current, forecast)
+  rl.close();
+});
 
-function getWeather(url){
-  request(url, function (error, response, body) {
+function forecastWeather(forecast){
+  request(forecast, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       var json = JSON.parse(body)
-      figlet(json.name, function(err, msg) {
-          if (err) {
-            console.log('error...');
-            console.dir(err);
-            return;
-          }
-          console.log(msg)
-          console.log(json.weather[0].description);
-          console.log("Current temperature: "+kelvinToF(json.main.temp)+"°F");
-          console.log("Wind blowing "+json.wind.speed+" MPH");
-          console.log('\n');
-      });
-      // console.log(json);
+      for (var i = 1; i < json.list.length; i++) {
+        var day = json.list[i]
+        console.log(moment.unix(day.dt).format('dddd, MMMM D'));
+        console.log('Low: '+day.temp.min+'°F');
+        console.log('High: '+day.temp.max+'°F');
+        console.log('\n');
+      }
     }
   })
 }
 
-function kelvinToF(kelvin){
-  return Math.round((kelvin*(9/5)) - 459.67)
+function getCurrentWeather(current, forecast){
+  request(current, function(error, response, body){
+    if (!error && response.statusCode == 200) {
+      var json = JSON.parse(body)
+      figlet(json.name, function(err, msg) {
+        console.log(msg);
+        console.log("Currently "+json.main.temp+"°F");
+        console.log(json.weather[0].description);
+        console.log(json.wind.speed+" MPH winds");
+        console.log("\n");
+      })
+    }
+  })
+  setTimeout(function(){
+    forecastWeather(forecast)
+  }, 800)
 }
